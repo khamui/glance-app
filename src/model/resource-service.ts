2d64e07513aa068dc6aa210790a5b245a7438fa1
Kha Tran <kha@desygner.com>
2020-02-16 20:03:11 +0000

    missing resource service file

---
+++ src/model/resource-service.ts
@@ -0,0 +1,27 @@
+import { inject } from 'aurelia-framework';
+import { ResourceList } from '../model/resource-list';
+
+export interface IResourcable {
+  resourcetype: string;
+  data?: any[][];
+  glaId?: number;
+  container?: HTMLElement;
+};
+
+@inject(ResourceList)
+export class ResourceService {
+  private rl : ResourceList;
+
+  constructor(resourceList: ResourceList) {
+    this.rl = resourceList;
+    console.log('new resource service initialized.');
+  }
+
+  getResourceItems() {
+    return this.rl.getItems();
+  }
+
+  registerInList(resource: IResourcable) {
+    this.rl.register(resource);
+  }
+}
