67f2fd6b364807e1c40fe68b5410244872570b6c
khamui <khamui@me.com>
2020-04-05 15:34:59 +0100

    Revert "component notification added - no backend part yet"
    This reverts commit dac7f110ffb5fac00e44ff82ae4c38c773e6c323.

--- src/editor/sheets.ts
+++ src/editor/sheets.ts
@@ -1,7 +1,6 @@
 import { inject, computedFrom } from 'aurelia-framework';
 import { SheetService } from './sheet-service';
 import { ResourceService } from '../model/resource-service';
-import { TNotification } from '../components/notification/notification';
 
 @inject(SheetService, ResourceService)
 export class Sheets {
@@ -10,20 +9,13 @@ export class Sheets {
   resourceListItems: any;
   glaId: number;
   sheetEls: NodeListOf<Element>;
-  notifications: TNotification[] = [];
 
-  @computedFrom(
-    'resourceListItems',
-    'resourceListItems[0].data.length',
-    'resourceListItems[1].data.length',
-  )
+  @computedFrom('resourceListItems','resourceListItems[0].data.length','resourceListItems[1].data.length')
   get divheight() {
-    const rowitemscount1 =
-      this.resourceListItems && this.resourceListItems[0].data.length;
-    const rowitemscount2 =
-      this.resourceListItems && this.resourceListItems[1].data.length;
-    return (rowitemscount1 + rowitemscount2) * 30 + 280;
-  }
+    const rowitemscount1 = this.resourceListItems && this.resourceListItems[0].data.length;
+    const rowitemscount2 = this.resourceListItems && this.resourceListItems[1].data.length;
+    return ((rowitemscount1 + rowitemscount2) * 30) + 280;
+  };
 
   constructor(sheetService: SheetService, resourceService: ResourceService) {
     this.ss = sheetService;
@@ -34,7 +26,7 @@ export class Sheets {
   async attached() {
     const items = await this.ss.load(this.glaId);
     for (let item of items) {
-      const values = await this.ss.loadValues(item);
+      const values = await this.ss.loadValues(item)
       const sheetData = this.mapSheetData(values);
       this.rs.registerInList({
         resourcetype: item.type,
@@ -43,29 +35,15 @@ export class Sheets {
       });
     }
     this.resourceListItems = this.rs.getResourceItems();
-    this.notifications.push(
-      {
-        dismissed: false,
-        dismissable: false,
-        text: 'This is a componentized notification',
-      },
-      {
-        dismissed: false,
-        dismissable: true,
-        text: 'Another one yay cool',
-      },
-    );
   }
 
   mapSheetData(values: object[]) {
-    return [
-      ...values.map((row) => {
-        let sheetData = [];
-        sheetData.push(row['cat_id']);
-        sheetData.push(row['sheet_id']);
-        sheetData.push(...JSON.parse(row['cat_data']));
-        return sheetData;
-      }),
-    ];
+    return [...values.map((row) => {
+      let sheetData = [];
+      sheetData.push(row['cat_id']);
+      sheetData.push(row['sheet_id']);
+      sheetData.push(...JSON.parse(row['cat_data']));
+      return sheetData;
+    })];
   }
 }
