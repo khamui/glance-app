ce8a369350e5dfcfca810fd42aa9846e205f0ce0
khamui <52801593+khamui@users.noreply.github.com>
2020-06-28 14:20:14 +0100

    Delete PARAMETERS.ts

--- src/PARAMETERS.ts
+++
@@ -1,9 +0,0 @@
-export const PARAMETERS = {
-    apiKey: "AIzaSyBtr6JWyQMOoCB53aT8SIj8QH60hxgjE4M",
-    authDomain: "glance-liquidity.firebaseapp.com",
-    databaseURL: "https://glance-liquidity.firebaseio.com",
-    projectId: "glance-liquidity",
-    storageBucket: "glance-liquidity.appspot.com",
-    messagingSenderId: "54961018549",
-    appId: "1:54961018549:web:ecacb270a7dfc57ca950fc"	
-}
